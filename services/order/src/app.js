f17dbe8ceefb863c0fcb483841ba9529fce3f96b
vinayellulla <vinaykumarellulla@gmail.com>
2026-07-24 16:36:45 +0530

    Rebuild images after infra recreation

--- services/order/src/app.js
+++ services/order/src/app.js
@@ -30,3 +30,4 @@ module.exports = app;
 // rebuild after infra recreate
 // rebuild after infra recreate
 // rebuild after infra recreate
+// rebuild after infra recreate
--- services/user/src/app.js
+++ services/user/src/app.js
@@ -32,3 +32,4 @@ module.exports = app;
 // rebuild after infra recreate
 // rebuild after infra recreate
 // rebuild after infra recreate
+// rebuild after infra recreate
